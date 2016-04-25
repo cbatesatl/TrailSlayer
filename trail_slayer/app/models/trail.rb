e9dca8e2802e6242ac7e325845e4b3c557068404
Chris Bates <Chrisbates79@icloud.com>
2016-04-25 14:57:12 -0400

    Added MVC CRUD for User

--- trail_slayer/app/models/trail.rb
+++ trail_slayer/app/models/trail.rb
@@ -1,2 +1,5 @@
 class Trail < ActiveRecord::Base
+
+has_many :users
 end
+
--- trail_slayer/config/routes.rb
+++ trail_slayer/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  root to: 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: 'get'
+  resources :home
   resources :trails
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
